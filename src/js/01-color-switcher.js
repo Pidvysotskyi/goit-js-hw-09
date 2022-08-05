62e21b4a107c8045b10eccb05f11054ef31e80e2
Oleksandr <Pidvysotskyi.oleksandr@gmail.com>
2022-08-05 15:59:58 +0300

    1st task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const bodyRef = document.querySelector('body');
+
+const startButtonRef = document.querySelector('button[data-start]');
+
+const stopButtonRef = document.querySelector('button[data-stop]');
+
+startButtonRef.addEventListener('click', onStartButtonClick);
+stopButtonRef.addEventListener('click', onStopButtonClick);
+
+let isButtonDisable = false;
+
+function onStartButtonClick() {
+  if (!isButtonDisable) {
+    timerID = setInterval(() => {
+      backColor = getRandomHexColor();
+      bodyRef.style.backgroundColor = backColor;
+      console.log(`I changed background collor for Body to ${backColor}`);
+      isButtonDisable = true;
+    }, 1000);
+  }
+}
+
+function onStopButtonClick() {
+  clearInterval(timerID);
+  isButtonDisable = false;
+}
