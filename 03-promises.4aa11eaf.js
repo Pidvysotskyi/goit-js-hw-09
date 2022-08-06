5b1b2cbeb4715451a3c6b2739e39f79db43c466d
Pidvysotskyi <100088935+Pidvysotskyi@users.noreply.github.com>
2022-08-06 12:52:35 +0000

    Deploying to gh-pages from @ Pidvysotskyi/goit-js-hw-09@a601e9b6c8ebb207e373b630326438db0150e3d0 🚀

---
+++ 03-promises.4aa11eaf.js
@@ -0,0 +1,2 @@
+new Promise(((o,e)=>{Math.random()>.3&&o("OK"),e("NOK")})).then((o=>{console.log(o)})).catch((o=>{console.log(o)}));
+//# sourceMappingURL=03-promises.4aa11eaf.js.map
---
+++ 03-promises.cbb2b7a5.js
@@ -0,0 +1,2 @@
+new Promise((function(n,o){Math.random()>.3&&n("OK"),o("NOK")})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
+//# sourceMappingURL=03-promises.cbb2b7a5.js.map
