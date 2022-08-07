524979358863d99a7f9e436ee59e1773519df39d
Oleksandr <Pidvysotskyi.oleksandr@gmail.com>
2022-08-07 15:30:22 +0300

    3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,17 +1,57 @@
 function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve('OK');
-    }
-    reject('NOK');
-  });
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({
+          position: `${position}`,
+          delay: `${delay}`,
+        });
+      }
+      reject({
+        position: `${position}`,
+        delay: `${delay}`,
+      });
+    });
+  }, 1000);
+  return promise;
 }
 
-createPromise()
-  .then(x => {
-    console.log(x);
-  })
-  .catch(y => {
-    console.log(y);
-  });
+console.log(
+  createPromise(1, 5000)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    })
+);
+
+// const firstDelayInputRef = document.querySelector('input[name = delay]');
+// const delayStepInputRef = document.querySelector('input[name = step]');
+// const amountInputRef = document.querySelector('input[name = amount]');
+// const submitFormRef = document.querySelector('.form');
+
+// submitFormRef.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(evt) {
+//   const firstDelay = Number(firstDelayInputRef.value);
+//   const delayStep = Number(delayStepInputRef.value);
+//   const amount = Number(amountInputRef.value);
+
+//   evt.preventDefault();
+
+//   let itemDelay = firstDelay;
+
+//   for (let i = 1; i <= amount; i += 1) {
+//     createPromise(i, itemDelay)
+//       .then(({ position, delay }) => {
+//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       })
+//       .catch(({ position, delay }) => {
+//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//       });
+//     itemDelay = firstDelay + i * delayStep;
+//   }
+// }
